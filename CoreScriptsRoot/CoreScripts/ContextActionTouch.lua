0ee89c25c52a1d6378c9f120da125ba0b00090f7
howmanysmall <9477871+1@iechs.org>
2017-11-16 14:10:49 -0700

    Update ContextActionTouch.lua

--- CoreScriptsRoot/CoreScripts/ContextActionTouch.lua
+++ CoreScriptsRoot/CoreScripts/ContextActionTouch.lua
@@ -3,29 +3,42 @@
 -- this script controls ui and firing of lua functions that are bound in ContextActionService for touch inputs
 -- Essentially a user can bind a lua function to a key code, input type (mousebutton1 etc.) and this
 
+local game = game
+local type = type
+local pairs = pairs
+
+local Instance = Instance local Instance_new = Instance.new
+local UDim2 = UDim2 local UDim2_new = UDim2.new
+local Color3 = Color3 local Color3_new = Color3.new
+
+local Enum = Enum
+	local UserInputState = Enum.UserInputState
+	local UserInputType = Enum.UserInputType
+	local Font = Enum.Font
+
 -- Variables
 local contextActionService = game:GetService("ContextActionService")
 local userInputService = game:GetService("UserInputService")
 local playersService = game:GetService("Players")
 local isTouchDevice = userInputService.TouchEnabled
-local functionTable = {}
-local buttonVector = {}
+local functionTable = { }
+local buttonVector = { }
 local buttonScreenGui = nil
 local buttonFrame = nil
 
-local ContextDownImage = "https://www.roblox.com/asset/?id=97166756"
-local ContextUpImage = "https://www.roblox.com/asset/?id=97166444"
+local ContextDownImage = "rbxassetid://97166756"
+local ContextUpImage = "rbxassetid://97166444"
 
-local oldTouches = {}
+local oldTouches = { }
 
 local buttonPositionTable = {	
-								[1] = UDim2.new(0,123,0,70), 
-								[2] = UDim2.new(0,30,0,60),
-								[3] = UDim2.new(0,180,0,160),
-								[4] = UDim2.new(0,85,0,-25),
-								[5] = UDim2.new(0,185,0,-25),
-								[6] = UDim2.new(0,185,0,260),
-								[7] = UDim2.new(0,216,0,65)
+								[1] = UDim2_new(0, 123, 0, 70), 
+								[2] = UDim2_new(0, 30, 0, 60),
+								[3] = UDim2_new(0, 180, 0, 160),
+								[4] = UDim2_new(0, 85, 0, -25),
+								[5] = UDim2_new(0, 185, 0, -25),
+								[6] = UDim2_new(0, 185, 0, 260),
+								[7] = UDim2_new(0, 216, 0, 65)
 							}
 local maxButtons = #buttonPositionTable
 
@@ -35,29 +48,29 @@ game:GetService("ContentProvider"):Preload(ContextUpImage)
 
 local localPlayer = playersService.LocalPlayer
 while not localPlayer do
-	playersService.ChildAdded:wait()
+	playersService.ChildAdded:Wait()
 	localPlayer = playersService.LocalPlayer
 end
 
 function createContextActionGui()
 	if not buttonScreenGui and isTouchDevice then
-		buttonScreenGui = Instance.new("ScreenGui")
+		buttonScreenGui = Instance_new("ScreenGui")
 		buttonScreenGui.Name = "ContextActionGui"
-		buttonScreenGui.AncestryChanged:connect(function(child, newParent)
+		buttonScreenGui.AncestryChanged:Connect(function(child, newParent)
 			if newParent == nil then
 				buttonScreenGui = nil
 			end
 		end)
 
-		buttonFrame = Instance.new("Frame")
+		buttonFrame = Instance_new("Frame")
 		buttonFrame.BackgroundTransparency = 1
-		buttonFrame.Size = UDim2.new(0.3,0,0.5,0)
-		buttonFrame.Position = UDim2.new(0.7,0,0.5,0)
+		buttonFrame.Size = UDim2_new(0.3, 0, 0.5, 0)
+		buttonFrame.Position = UDim2_new(0.7, 0, 0.5, 0)
 		buttonFrame.Name = "ContextButtonFrame"
 		buttonFrame.Parent = buttonScreenGui
 
 		buttonFrame.Visible = not userInputService.ModalEnabled
-		userInputService.Changed:connect(function(property)
+		userInputService.Changed:Connect(function(property)
 			if property == "ModalEnabled" then
 				buttonFrame.Visible = not userInputService.ModalEnabled
 			end
@@ -77,28 +90,27 @@ function setButtonSizeAndPosition(object)
 		buttonSize = 85
 		xOffset = 40
 	end
-
-	object.Size = UDim2.new(0,buttonSize,0,buttonSize)
+	object.Size = UDim2_new(0, buttonSize, 0, buttonSize)
 end
 
 function contextButtonDown(button, inputObject, actionName)
-	if inputObject.UserInputType == Enum.UserInputType.Touch then
+	if inputObject.UserInputType == UserInputType.Touch then
 		button.Image = ContextDownImage
-		contextActionService:CallFunction(actionName, Enum.UserInputState.Begin, inputObject)
+		contextActionService:CallFunction(actionName, UserInputState.Begin, inputObject)
 	end
 end
 
 function contextButtonMoved(button, inputObject, actionName)
-	if inputObject.UserInputType == Enum.UserInputType.Touch then
+	if inputObject.UserInputType == UserInputType.Touch then
 		button.Image = ContextDownImage
-		contextActionService:CallFunction(actionName, Enum.UserInputState.Change, inputObject)
+		contextActionService:CallFunction(actionName, UserInputState.Change, inputObject)
 	end
 end
 
 function contextButtonUp(button, inputObject, actionName)
 	button.Image = ContextUpImage
-	if inputObject.UserInputType == Enum.UserInputType.Touch and inputObject.UserInputState == Enum.UserInputState.End then
-		contextActionService:CallFunction(actionName, Enum.UserInputState.End, inputObject)
+	if inputObject.UserInputType == UserInputType.Touch and inputObject.UserInputState == UserInputState.End then
+		contextActionService:CallFunction(actionName, UserInputState.End, inputObject)
 	end
 end
 
@@ -108,37 +120,29 @@ end
 
 
 function createNewButton(actionName, functionInfoTable)
-	local contextButton = Instance.new("ImageButton")
+	local contextButton = Instance_new("ImageButton")
 	contextButton.Name = "ContextActionButton"
 	contextButton.BackgroundTransparency = 1
-	contextButton.Size = UDim2.new(0,45,0,45)
+	contextButton.Size = UDim2_new(0, 45, 0, 45)
 	contextButton.Active = true
-	if isSmallScreenDevice() then 
-		contextButton.Size = UDim2.new(0,35,0,35)
-	end
+	if isSmallScreenDevice() then contextButton.Size = UDim2_new(0, 35, 0, 35) end
 	contextButton.Image = ContextUpImage
 	contextButton.Parent = buttonFrame
-
 	local currentButtonTouch = nil
-
-	userInputService.InputEnded:connect(function ( inputObject )
-		oldTouches[inputObject] = nil
-	end)
-	contextButton.InputBegan:connect(function(inputObject)
+	userInputService.InputEnded:Connect(function(inputObject) oldTouches[inputObject] = nil end)
+	contextButton.InputBegan:Connect(function(inputObject)
 		if oldTouches[inputObject] then return end
-
-		if inputObject.UserInputState == Enum.UserInputState.Begin and currentButtonTouch == nil then
+		if inputObject.UserInputState == UserInputState.Begin and currentButtonTouch == nil then
 			currentButtonTouch = inputObject
 			contextButtonDown(contextButton, inputObject, actionName)
 		end
 	end)
-	contextButton.InputChanged:connect(function(inputObject)
+	contextButton.InputChanged:Connect(function(inputObject)
 		if oldTouches[inputObject] then return end
 		if currentButtonTouch ~= inputObject then return end
-
 		contextButtonMoved(contextButton, inputObject, actionName)
 	end)
-	contextButton.InputEnded:connect(function(inputObject)
+	contextButton.InputEnded:Connect(function(inputObject)
 		if oldTouches[inputObject] then return end
 		if currentButtonTouch ~= inputObject then return end
 
@@ -147,24 +151,25 @@ function createNewButton(actionName, functionInfoTable)
 		contextButtonUp(contextButton, inputObject, actionName)
 	end)
 
-	local actionIcon = Instance.new("ImageLabel")
+	local actionIcon = Instance_new("ImageLabel")
 	actionIcon.Name = "ActionIcon"
-	actionIcon.Position = UDim2.new(0.175, 0, 0.175, 0)
-	actionIcon.Size = UDim2.new(0.65, 0, 0.65, 0)
+	actionIcon.Position = UDim2_new(0.175, 0, 0.175, 0)
+	actionIcon.Size = UDim2_new(0.65, 0, 0.65, 0)
 	actionIcon.BackgroundTransparency = 1
 	if functionInfoTable["image"] and type(functionInfoTable["image"]) == "string" then
 		actionIcon.Image = functionInfoTable["image"]
 	end
 	actionIcon.Parent = contextButton
 
-	local actionTitle = Instance.new("TextLabel")
+	local actionTitle = Instance_new("TextLabel")
 	actionTitle.Name = "ActionTitle"
-	actionTitle.Size = UDim2.new(1,0,1,0)
+	actionTitle.Size = UDim2_new(1, 0, 1, 0)
 	actionTitle.BackgroundTransparency = 1
-	actionTitle.Font = Enum.Font.SourceSansBold
-	actionTitle.TextColor3 = Color3.new(1,1,1)
+	actionTitle.Font = Font.SourceSansBold
+	actionTitle.TextColor3 = Color3_new(1, 1, 1)
 	actionTitle.TextStrokeTransparency = 0
-	actionTitle.FontSize = Enum.FontSize.Size18
+	actionTitle.TextSize = 18
+--	actionTitle.FontSize = FontSize.Size18
 	actionTitle.TextWrapped = true
 	actionTitle.Text = ""
 	if functionInfoTable["title"] and type(functionInfoTable["title"]) == "string" then
@@ -175,11 +180,11 @@ function createNewButton(actionName, functionInfoTable)
 	return contextButton
 end
 
-function createButton( actionName, functionInfoTable )
+function createButton(actionName, functionInfoTable)
 	local button = createNewButton(actionName, functionInfoTable)
 
 	local position = nil
-	for i = 1,#buttonVector do
+	for i = 1, #buttonVector do
 		if buttonVector[i] == "empty" then
 			position = i
 			break
@@ -216,7 +221,7 @@ function removeAction(actionName)
 	if actionButton then
 		actionButton.Parent = nil
 
-		for i = 1,#buttonVector do
+		for i = 1, #buttonVector do
 			if buttonVector[i] == actionButton then
 				buttonVector[i] = "empty"
 				break
@@ -229,11 +234,11 @@ function removeAction(actionName)
 	functionTable[actionName] = nil
 end
 
-function addAction(actionName,createTouchButton,functionInfoTable)
+function addAction(actionName, createTouchButton, functionInfoTable)
 	if functionTable[actionName] then
 		removeAction(actionName)
 	end
-	functionTable[actionName] = {functionInfoTable}
+	functionTable[actionName] = { functionInfoTable }
 	if createTouchButton and isTouchDevice then
 		createContextActionGui()
 		createButton(actionName, functionInfoTable)
@@ -241,7 +246,7 @@ function addAction(actionName,createTouchButton,functionInfoTable)
 end
 
 -- Connections
-contextActionService.BoundActionChanged:connect( function(actionName, changeName, changeTable)
+contextActionService.BoundActionChanged:Connect(function(actionName, changeName, changeTable)
 	if functionTable[actionName] and changeTable then
 		local button = functionTable[actionName]["button"]
 		if button then
@@ -258,15 +263,15 @@ contextActionService.BoundActionChanged:connect( function(actionName, changeName
 	end
 end)
 
-contextActionService.BoundActionAdded:connect( function(actionName, createTouchButton, functionInfoTable)
+contextActionService.BoundActionAdded:Connect(function(actionName, createTouchButton, functionInfoTable)
 	addAction(actionName, createTouchButton, functionInfoTable)
 end)
 
-contextActionService.BoundActionRemoved:connect( function(actionName, functionInfoTable)
+contextActionService.BoundActionRemoved:Connect(function(actionName, functionInfoTable)
 	removeAction(actionName)
 end)
 
-contextActionService.GetActionButtonEvent:connect( function(actionName)
+contextActionService.GetActionButtonEvent:Connect(function(actionName)
 	if functionTable[actionName] then
 		contextActionService:FireActionButtonFoundSignal(actionName, functionTable[actionName]["button"])
 	end
@@ -275,5 +280,5 @@ end)
 -- make sure any bound data before we setup connections is handled
 local boundActions = contextActionService:GetAllBoundActionInfo()
 for actionName, actionData in pairs(boundActions) do
-	addAction(actionName,actionData["createTouchButton"],actionData)
+	addAction(actionName,actionData["createTouchButton"], actionData)
 end

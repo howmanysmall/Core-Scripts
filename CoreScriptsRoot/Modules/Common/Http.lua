abe446eb185f493ee3b1f6b7208132d46b90a9ed
howmanysmall <9477871+1@iechs.org>
2017-11-17 12:30:09 -0700

    Update Http.lua

--- CoreScriptsRoot/Modules/Common/Http.lua
+++ CoreScriptsRoot/Modules/Common/Http.lua
@@ -1,117 +1,82 @@
 --[[
-                // Http.lua
-
-                // API for making http calls into roblox endpoints
+	// Http.lua
+	// API for making http calls into roblox endpoints
 ]]
+local game = game
+local pcall = pcall
+local print = print
+
 local HttpService = game:GetService('HttpService')
 local HttpRbxApiService = game:GetService('HttpRbxApiService')
 
-local Http = {} -- module return table
+local Http = { } -- module return table
 
 local BaseUrl = game:GetService('ContentProvider').BaseUrl:lower()
-BaseUrl = string.gsub(BaseUrl, "/m.", "/www.")
-BaseUrl = string.gsub(BaseUrl, "http://", "https://")
+BaseUrl = BaseUrl:gsub("/m.", "/www.")
+BaseUrl = BaseUrl:gsub("http://", "https://")
 
-local AssetGameBaseUrl = string.gsub(BaseUrl, "https://www.", "https://assetgame.")
-
-
-function Http.GetBaseUrl()
-    return BaseUrl
-end
-function Http.GetAssetGameBaseUrl()
-    return AssetGameBaseUrl
-end
-
+local AssetGameBaseUrl = BaseUrl:gsub("https://www.", "https://assetgame.")
+function Http.GetBaseUrl() return BaseUrl end
+function Http.GetAssetGameBaseUrl() return AssetGameBaseUrl end
 function Http.DecodeJSON(json)
-    local success, result = pcall(function()
-        return HttpService:JSONDecode(json)
-    end)
-    if not success then
-        print("Http.DecodeJSON() failed because", result, "for input:", json)
-        return
-    end
-
-    return result
+	local success, result = pcall(function() return HttpService:JSONDecode(json) end)
+	if not success then
+		print("Http.DecodeJSON() failed because", result, "for input:", json)
+		return
+	end
+	return result
 end
 
 function Http.EncodeJSON(jsonTable)
-    local success, result = pcall(function()
-        return HttpService:JSONEncode(jsonTable)
-    end)
-    if not success then
-        print("Http.EncodeJSON() failed because", result)
-        return
-    end
-
-    return result
+	local success, result = pcall(function() return HttpService:JSONEncode(jsonTable) end)
+	if not success then
+		print("Http.EncodeJSON() failed because", result)
+		return
+	end
+	return result
 end
 
 function Http.RbxGetAsync(path, returnRaw)
-    local success, result = pcall(function()
-        return game:HttpGetAsync(path)
-    end)
-    if not success then
-        print("Http.RbxGetAsync() failed because", result, "for path:", path)
-        return
-    end
-
-    if returnRaw then
-        return result
-    end
-    
-    return Http.DecodeJSON(result)
+	local success, result = pcall(function() return game:HttpGetAsync(path) end)
+	if not success then
+		print("Http.RbxGetAsync() failed because", result, "for path:", path)
+		return
+	end
+	if returnRaw then return result end
+	return Http.DecodeJSON(result)
 end
 
 function Http.RbxPostAsync(path, returnRaw)
-    local success, result = pcall(function()
-        return game:HttpPostAsync(path, '')
-    end)
-    if not success then
-        print("Http.RbxPostAsync() failed because", result, "for path:", path)
-        return
-    end
-
-    if returnRaw then
-        return result
-    end
-
-    return Http.DecodeJSON(result)
+	local success, result = pcall(function() return game:HttpPostAsync(path, '') end)
+	if not success then
+		print("Http.RbxPostAsync() failed because", result, "for path:", path)
+		return
+	end
+	if returnRaw then return result end
+	return Http.DecodeJSON(result)
 end
 
 function Http.RbxApiGetAsync(path, throttlePriority, returnRaw)
-    -- throttlePriority: defaults to Enum.ThrottlingPriority.Default through reflection
-    local success, result = pcall(function()
-        return HttpRbxApiService:GetAsync(path, throttlePriority)
-    end)
-    if not success then
-        print("Http.RbxApiGetAsync() failed because", result, "for path:", path)
-        return
-    end
-
-    if returnRaw then
-        return result
-    end
-
-    return Http.DecodeJSON(result)
+	-- throttlePriority: defaults to Enum.ThrottlingPriority.Default through reflection
+	local success, result = pcall(function() return HttpRbxApiService:GetAsync(path, throttlePriority) end)
+	if not success then
+		print("Http.RbxApiGetAsync() failed because", result, "for path:", path)
+		return
+	end
+	if returnRaw then return result end
+	return Http.DecodeJSON(result)
 end
 
 function Http.RbxApiPostAsync(path, params, throttlePriority, contentType, returnRaw)
-    -- throttlePriority: defaults to Enum.ThrottlingPriority.Default through reflection
-    -- contentType: defaults to Enum.HttpContentType.ApplicationJson through reflection
-    local success, result = pcall(function()
-        return HttpRbxApiService:PostAsync(path, params, throttlePriority, contentType)
-    end)
-
-    if not success then
-        print("Http.RbxApiPostAsync() failed because", result, "for path:", path)
-        return
-    end
-
-    if returnRaw then
-        return result
-    end
-
-    return Http.DecodeJSON(result)
+	-- throttlePriority: defaults to Enum.ThrottlingPriority.Default through reflection
+	-- contentType: defaults to Enum.HttpContentType.ApplicationJson through reflection
+	local success, result = pcall(function() return HttpRbxApiService:PostAsync(path, params, throttlePriority, contentType) end)
+	if not success then
+		print("Http.RbxApiPostAsync() failed because", result, "for path:", path)
+		return
+	end
+	if returnRaw then return result end
+	return Http.DecodeJSON(result)
 end
 
 return Http

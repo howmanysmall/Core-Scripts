8614a210207b0b2cd8eecf12b5f82c1fa1d70393
howmanysmall <9477871+1@iechs.org>
2017-11-17 12:24:43 -0700

    Update CommonUtil.lua

--- CoreScriptsRoot/Modules/Common/CommonUtil.lua
+++ CoreScriptsRoot/Modules/Common/CommonUtil.lua
@@ -1,19 +1,20 @@
 --[[
-  Filename: CommonUtil.lua
-  Written by: dbanks
-  Description: Common work.
+	Filename: CommonUtil.lua
+	Written by: dbanks
+	Description: Common work.
 --]]
-
+local table = table local sort = table.sort
 
 --[[ Classes ]]--
-local CommonUtil = {}
-      
+local CommonUtil = { }
+
 -- Concatenate these two tables, return result.
-function CommonUtil.TableConcat(t1,t2)
-    for i=1,#t2 do
-        t1[#t1+1] = t2[i]
-    end
-    return t1
+function CommonUtil.TableConcat(t1, t2)
+	local t1len, t2len = #t1, #t2
+	for i = 1, t2len do
+		t1[t1len + 1] = t2[i]
+	end
+	return t1
 end
 
 -- Instances have a "Name" field.  Sort 
@@ -22,8 +23,8 @@ function CommonUtil.SortByName(items)
 	function compareInstanceNames(i1, i2) 
 		return (i1.Name < i2.Name)
 	end
-	table.sort(items, compareInstanceNames)
+	sort(items, compareInstanceNames)
 	return items
 end
-	
+
 return CommonUtil
